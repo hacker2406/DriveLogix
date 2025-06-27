257e5321787d79dc4be8a0515ee9feeca19a78b3
hacker2406 <ratul.9paul@gmail.com>
2025-06-27 13:28:16 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js"
+import connectDB from "./config/db.js"
+import cors from "cors"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Shoe E-Commerce Backend is Running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
